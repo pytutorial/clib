07539cd07438dd8cf96752ba3dd52511105aed7a
Tung Duong <duongthanhtungvn01@gmail.com>
2020-07-16 10:14:21 +0100

    Update

--- src/test_list.c
+++ src/test_list.c
@@ -1,6 +1,36 @@
 #include "list.h"
 #include <stdio.h>
 
+#define _quicksort(number,N, func){             \
+   int i, j, pivot, temp;                       \
+   if(N-1>0){                                   \
+      pivot=0;                                  \
+      i=0;                                      \
+      j=N-1;                                    \
+      while(i<j){                               \
+         while(number[i]<=number[pivot]&&i<N-1) \
+            i++;                                \
+         while(number[j]>number[pivot])         \
+            j--;                                \
+         if(i<j){                               \
+            temp=number[i];                     \
+            number[i]=number[j];                \
+            number[j]=temp;                     \
+         }                                      \
+      }                                         \
+      temp=number[pivot];                       \
+      number[pivot]=number[j];                  \
+      number[j]=temp;                           \
+      func(number,j);                           \
+      func(number+j+1,N-j-1);                   \
+   }                                            \
+}
+
+void quicksort(int* number, int N) 
+{
+    _quicksort(number, N, quicksort);
+}
+
 int main()
 {
     Scope scope = newScope();
@@ -9,11 +39,12 @@ int main()
     listPush(lst, 3);
     listPush(lst, 8);
     listPush(lst, 6);
+    quicksort(lst->items, listSize(lst));
     printList(lst, "%d");
-    printf("Argmax: %d\n", listArgMax(lst));
+    /*printf("Argmax: %d\n", listArgMax(lst));
     printf("Max: %d\n", listMax(lst));
     printf("Argmin: %d\n", listArgMin(lst));
-    printf("Min: %d\n", listMin(lst));
+    printf("Min: %d\n", listMin(lst));*/
     freeScope(scope);
     return 0;
 }
